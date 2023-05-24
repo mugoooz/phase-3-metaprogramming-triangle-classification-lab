3f7bcf26f3963d4f0c4b2e725dd62768e672a605
Eric Mugo <mugoericpg20@gmail.com>
2023-05-24 13:56:37 +0300

    Added conditionals

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,19 @@ class Triangle
   end
 
   def kind
+    if @side1 == side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    else
+      :scalene
+    end
     
   end
+
+  private
+  def 
+
+
+
 end
