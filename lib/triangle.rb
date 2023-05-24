22298b315c32411ac2b0d7324e5cbb781f71b198
Eric Mugo <mugoericpg20@gmail.com>
2023-05-24 14:03:17 +0300

    Completed Lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,27 +1,32 @@
 class Triangle
-  def initialize(l1,l2,l3)
-    @side1 = l1
-    @side2 = l2
-    @side3 = l3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind
-    if @side1 == side2 && @side2 == @side3
+    validate_triangle
+
+    if @side1 == @side2 && @side2 == @side3
       :equilateral
+    
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
       :isosceles
+
     else
       :scalene
     end
-    
   end
 
   private
 
   def validate_triangle
     sides = [@side1, @side2, @side3]
+
     raise TriangleError, "All sides must be larger than 0" if sides.any? { |side| side <= 0 }
     raise TriangleError, "Invalid triangle: the sum of any two sides must exceed the length of the third side" unless triangle_inequality?
+  
   end
 
   def triangle_inequality?
@@ -30,8 +35,4 @@ class Triangle
 
   class TriangleError < StandardError
   end
-
-
-
-
 end
