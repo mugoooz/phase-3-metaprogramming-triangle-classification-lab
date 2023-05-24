379fae75b1ed8614e835e8d763424ce4c839845c
Eric Mugo <mugoericpg20@gmail.com>
2023-05-24 13:59:18 +0300

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,20 @@ class Triangle
   end
 
   private
-  def 
+
+  def validate_triangle
+    sides = [@side1, @side2, @side3]
+    raise TriangleError, "All sides must be larger than 0" if sides.any? { |side| side <= 0 }
+    raise TriangleError, "Invalid triangle: the sum of any two sides must exceed the length of the third side" unless triangle_inequality?
+  end
+
+  def triangle_inequality?
+    @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side1 + @side3 > @side2
+  end
+
+  class TriangleError < StandardError
+  end
+
 
 
 
